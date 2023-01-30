7d38897323a1b730ba088192bce4bc47e09d43f6
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-01-30 10:18:41 +0100

    Test Driven Development

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,17 @@
+// Greet should render the text hello and if a name is passed into the component
+// It should render hello followed by the name
+
 import { render, screen } from '@testing-library/react';
 import Greet from './greet';
 
 test('Greet renders correctly', () => {
   render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
+  const textElement = screen.getByText('Hello');
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Greet renders with a name', () => {
+  render(<Greet name="John" />);
+  const textElement = screen.getByText('Hello John');
   expect(textElement).toBeInTheDocument();
 });
