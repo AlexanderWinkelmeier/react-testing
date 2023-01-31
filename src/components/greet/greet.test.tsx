d8320f8c82339a59b14103d8f17b79ffcf4a71e3
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-01-31 09:41:53 +0100

    Code Coverage

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -7,15 +7,15 @@ import Greet from './greet';
 describe('Greet', () => {
   test('renders correctly', () => {
     render(<Greet />);
-    const textElement = screen.getByText('Hello');
+    const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
   });
 });
 
-describe('Nested', () => {
-  test('renders with a name', () => {
-    render(<Greet name="John" />);
-    const textElement = screen.getByText('Hello John');
-    expect(textElement).toBeInTheDocument();
-  });
-});
+// describe('Nested', () => {
+//   test('renders with a name', () => {
+//     render(<Greet name="John" />);
+//     const textElement = screen.getByText('Hello John');
+//     expect(textElement).toBeInTheDocument();
+//   });
+// });
