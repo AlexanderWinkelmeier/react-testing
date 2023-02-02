711996aa24be78473cec67bdbbd530891971a3b2
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-02-02 08:17:55 +0100

    getByRole Options

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -5,9 +5,31 @@ describe('Application', () => {
   test('renders correctly', () => {
     render(<Application />);
 
+    // prüft die Existenz einer Überschrift mit dem Level 1, d.h. h1-Tag
+    const pageHeading = screen.getByRole('heading', {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    // prüft die Existen einer Überschrift mit dem Level 2, d.h. h2-Tag
+    const sectionHeading = screen.getByRole('heading', {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
     // prüft die Existenz des Input-Elements mit type="text"
-    const nameElement = screen.getByRole('textbox');
+    // UND dem Label mit dem Inhalt 'Name'
+    const nameElement = screen.getByRole('textbox', {
+      name: 'Name',
+    });
     expect(nameElement).toBeInTheDocument();
+
+    // prüft die Existenz des Text-Area-Elements (hat die Rolle 'textbox'!)
+    // mit dem Label-Inhalt 'Bio'
+    const bioElement = screen.getByRole('textbox', {
+      name: 'Bio',
+    });
+    expect(bioElement).toBeInTheDocument();
 
     // prüft die Existenz des Select-Elements
     const jobLocationElement = screen.getByRole('combobox');
