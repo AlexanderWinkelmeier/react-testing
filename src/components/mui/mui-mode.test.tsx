b92bdec2f8debc7fd5be4c4d20a4d8bf0060ca9a
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-02-06 06:34:02 +0100

    Custom Render Functions

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,12 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../test-utils';
 import { MuiMode } from './mui-mode';
-import { AppProviders } from '../../providers/app-providers';
 
 describe('MuiMode', () => {
   test('renders text correctly', () => {
-    // <MuiMode /> befindet sich im wrapper AppProviders
-    // bevor MuiMode gerendert wird, wird es in AppProvider gewrappt
-    render(<MuiMode />, { wrapper: AppProviders });
+    render(<MuiMode />);
     const headingElement = screen.getByRole('heading');
     // prüft, ob das h-Tag den innerHtml-Content 'dark mode' hat
     expect(headingElement).toHaveTextContent('dark mode');
