8975a6a4a4bf3e15f91a3022607df148f29a9784
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-02-07 07:57:26 +0100

    MSW Error Handling

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Users } from './users';
+import { server } from '../../mocks/server';
+import { rest } from 'msw';
 
 describe('Users', () => {
   test('renders correctly', () => {
@@ -8,9 +10,24 @@ describe('Users', () => {
     const textElement = screen.getByText('Users');
     expect(textElement).toBeInTheDocument();
   });
+  // Mocking http-Request mit MSW
   test('renders a list of users', async () => {
     render(<Users />);
     const users = await screen.findAllByRole('listitem');
-    expect(users).toHaveLength(4); // 3 ist die Länge des Mock-Response!
+    expect(users).toHaveLength(3); // 3 ist die Länge des Mock-Response!
+  });
+  // prüfen, ob das Error-Handling funktioniert
+  test('renders error', async () => {
+    server.use(
+      rest.get(
+        'https://jsonplaceholder.typicode.com/users',
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const error = await screen.findByText('Error fetching users'); // Zeile 15 in users.tsx
+    expect(error).toBeInTheDocument();
   });
 });
