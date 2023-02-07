7c8f46afe72c9400ab92a3a86761914aed717cc8
AlexanderWinkelmeier <41574342+AlexanderWinkelmeier@users.noreply.github.com>
2023-02-07 07:30:50 +0100

    TestingTesting with MSW

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+// API-Mocking vor allen Tests einrichten
+beforeAll(() => server.listen());
+
+// Setzt alle Request-Handler zurück, die wir während der Tests hinzufügen können, damit sie sich nicht auf andere Tests auswirken können
+afterEach(() => server.resetHandlers());
+
+// Säubert nach Abschluss der Tests
+afterAll(() => server.close());
